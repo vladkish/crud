160b9bc78d1ca02777ba521b81d17dfd680d800c
vladkish <vladkish@users.noreply.github.com>
2025-07-04 15:06:09 +0300

    Creating objects and working with the admin panel, creating a superuser

--- backend/poem/show/models.py
+++ backend/poem/show/models.py
@@ -3,8 +3,14 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=60)
     
+    def __str__(self):
+        return f'{self.title}'
+    
 class Poem(models.Model):
     title = models.CharField(max_length=120)
     text = models.TextField()
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='poems')
     date_public = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.title}, {self.text} == {self.category}'
