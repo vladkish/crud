7ccc3e8f01d15f80f47f76da6980d9144c4b1e23
vladkish <vladkish@users.noreply.github.com>
2025-07-04 14:55:53 +0300

    Create models Category, Poem

--- backend/poem/show/models.py
+++ backend/poem/show/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=60)
+    
+class Poem(models.Model):
+    title = models.CharField(max_length=120)
+    text = models.TextField()
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='poems')
+    date_public = models.DateTimeField(auto_now_add=True)
