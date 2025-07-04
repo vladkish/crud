0e7522bee00619d4c490fa775a48e0030e09da81
vladkish <vladkish@users.noreply.github.com>
2025-07-04 17:46:05 +0300

    Display categories and latest objects of class Poem

--- backend/poem/show/views.py
+++ backend/poem/show/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Category, Poem
 
 def index(request):
-    return render(request, 'show/index.html')
+    
+    context = {
+        'categoryies' : Category.objects.all(),
+        'last_poem' : Poem.objects.all().order_by('-id')[:3]
+    }
+    
+    return render(request, 'show/index.html', context)
 
 def poem(request):
     return render(request, 'show/poem.html')
