d22add7541ef4b4731ccbf9b32b0744d33d9bc9d
vladkish <vladkish@users.noreply.github.com>
2025-07-06 23:04:03 +0300

    create model user

--- backend/poem/poem/settings.py
+++ backend/poem/poem/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'show'
+    'show',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
