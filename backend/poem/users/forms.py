b335daecaa8cb90cf8de7974162cbb432c5149b3
vladkish <vladkish@users.noreply.github.com>
2025-07-07 11:03:03 +0300

    Add custom in form SignForm

--- backend/poem/users/forms.py
+++ backend/poem/users/forms.py
@@ -5,7 +5,26 @@ from .models import User
 class LoginForm(AuthenticationForm):
     pass
 
+# Custom form
 class SignForm(UserCreationForm):
+    
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder' : 'Ваше имя' 
+    }))
+    
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'placeholder' : "Ваш email"
+    }))
+    
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : "Придумайте пароль"
+    }))
+    
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : "Повторите пароль"
+    }))
+        
+    
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
