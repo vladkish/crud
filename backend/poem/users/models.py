7b9b07f1ae941000334f7b29fadccd7587d6c0b8
vladkish <vladkish@users.noreply.github.com>
2025-07-09 13:06:22 +0300

    Create a default user avatar

--- backend/poem/users/models.py
+++ backend/poem/users/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Main user.
 class User(AbstractUser):
-    image = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    image = models.ImageField(upload_to='avatars/', blank=True, null=True, default='avatars/author.jpg')
     about_me = models.TextField(null=True, blank=True)
