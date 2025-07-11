3768696900b080276a7c13a0a95f2a9a97634446
vladkish <vladkish@users.noreply.github.com>
2025-07-11 13:04:41 +0300

    Writing a function to copy a link to a poem

--- backend/poem/frontend/js/main.js
+++ backend/poem/frontend/js/main.js
@@ -10,3 +10,21 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("copy-link-btn");
+    const confirm = document.getElementById("copy-confirm");
+
+    btn.addEventListener("click", function () {
+        const url = window.location.href;
+        navigator.clipboard.writeText(url).then(() => {
+            confirm.classList.add("show");
+
+            setTimeout(() => {
+                confirm.classList.remove("show");
+            }, 2000);
+        }).catch(() => {
+            alert("Не удалось скопировать ссылку.");
+        });
+    });
+});
